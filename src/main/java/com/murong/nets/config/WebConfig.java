be993a5c74d7f7f4d600eab9022af495466c5a0e
yaochuang <chuang.yao1081@seaboxdata.com>
2024-05-09 17:54:13 +0800

    时间戳

--- src/main/java/com/murong/nets/config/WebConfig.java
+++ src/main/java/com/murong/nets/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.murong.nets.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -19,7 +20,7 @@ public class WebConfig {
      */
     @Bean
     public ObjectMapper objectMapper() {
-        return Jackson2ObjectMapperBuilder.json().serializationInclusion(JsonInclude.Include.NON_NULL).build();
+        return Jackson2ObjectMapperBuilder.json().serializationInclusion(JsonInclude.Include.NON_NULL).featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).build();
     }
 
 }
